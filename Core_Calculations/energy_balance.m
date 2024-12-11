806b473e8802ff8ed7683b7e0449de54793d2c57
maimoh <maimoh85349@gmail.com>
2024-12-11 23:19:50 +0200

    Update energy_balance.m

--- Core_Calculations/energy_balance.m
+++ Core_Calculations/energy_balance.m
@@ -4,13 +4,7 @@ function [Q] = energy_balance(mdot_feed, h_feed, mdot_distillate, h_distillate, 
 end
 
 
-% تعريف المدخلات
-(* mdot_feed = 100;       % معدل تدفق التغذية (kg/s)
-h_feed = 500;          % الإنثالبي للتغذية (kJ/kg)
-mdot_distillate = 60;  % معدل المنتج العلوي (kg/s)
-h_distillate = 800;    % الإنثالبي للمنتج العلوي (kJ/kg)
-mdot_bottom = 40;      % معدل المنتج السفلي (kg/s)
-h_bottom = 300;        % الإنثالبي للمنتج السفلي (kJ/kg) *)
+
 
 % استدعاء الدالة
 Q = energy_balance(mdot_feed, h_feed, mdot_distillate, h_distillate, mdot_bottom, h_bottom);
